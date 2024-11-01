a24bcf681c121a8bda820203063a7292b323f2bb
Redbeard93 <67786490+Redbeard93@users.noreply.github.com>
2024-11-02 03:16:38 +0800

    Update colorscheme.lua

--- colorscheme.lua
+++ colorscheme.lua
@@ -11,11 +11,11 @@ return {
       on_highlights = function(hl, c)
         local prompt = "#2d3149"
         hl.TelescopeNormal = {
-          bg = c.bg_dark,
+          bg = "NONE",
           fg = c.fg_dark,
         }
         hl.TelescopeBorder = {
-          bg = c.bg_dark,
+          bg = "NONE",
           fg = c.bg_dark,
         }
         hl.TelescopePromptNormal = {
@@ -30,11 +30,11 @@ return {
           fg = prompt,
         }
         hl.TelescopePreviewTitle = {
-          bg = c.bg_dark,
+          bg = "NONE",
           fg = c.bg_dark,
         }
         hl.TelescopeResultsTitle = {
-          bg = c.bg_dark,
+          bg = "NONE",
           fg = c.bg_dark,
         }
         hl.Normal = {
