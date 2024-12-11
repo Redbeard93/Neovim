90357f59777afdb91c8d471843675480325eefb1
Redbeard93 <67786490+Redbeard93@users.noreply.github.com>
2024-12-11 15:01:23 +0800

    Update indent-blankline.lua

--- indent-blankline.lua
+++ indent-blankline.lua
@@ -28,16 +28,6 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = "LazyFile",
     opts = function()
-      LazyVim.toggle.map("<leader>ug", {
-        name = "Indention Guides",
-        get = function()
-          return require("ibl.config").get_config(0).enabled
-        end,
-        set = function(state)
-          require("ibl").setup_buffer(0, { enabled = state })
-        end,
-      })
-
       return {
         scope = { show_start = false, show_end = false },
         exclude = {
